85ad85932aa4d6edf2e188563410fee5f1fcda1b
nerocrux <nerocrux@gmail.com>
2013-09-08 15:11:37 +0900

    remove mayuyustats

--- camera.sh
+++ camera.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-fswebcam -r 960x720 -d /dev/video0 webcam.jpg
+fswebcam -r 960x720 -d /dev/video1 webcam.jpg
 sleep 10
 python /home/pi/Work/wayterm tweet "私はいまこんな景色をみてるよ。今日も早く帰ってきてね♡" "webcam.jpg"
